263ea29d037b4b168bcb96f41da66e39096d5633
Sumit Sontakke <smsontakke@ymail.com>
2022-02-20 20:27:30 +0000

    docker file added

--- django-project/mysite/settings.py
+++ django-project/mysite/settings.py
@@ -26,9 +26,9 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
