71507e7e3dba5077af1d018c906753605e98d821
Er-sumit <41416816+Er-sumit@users.noreply.github.com>
2022-05-21 09:56:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,32 @@ terraform {
 }
 #
 # An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
+#resource "null_resource" "example" {
+#  triggers = {
+#    value = "A example resource that does nothing!"
+#  }
+#}
+
+#DEPLOY VPC
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "network" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev1"
   }
 }
