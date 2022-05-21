e1ccb5b055aa5b43c765a1ee9a670e7d45399817
Sumit Sontakke <smsontakke@ymail.com>
2022-05-21 11:02:27 +0100

    tf env updated

--- main.tf
+++ main.tf
@@ -1,29 +1,9 @@
 terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
+  cloud {
     organization = "own-space"
-    workspaces { prefix = "open-source-" }
-  }
-}
 
-provider "aws" {
-  region = "ap-south-1"
-}
-
-module "network" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs            = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev1"
+    workspaces {
+      name = "Django-gh-actions-test"
+    }
   }
 }
