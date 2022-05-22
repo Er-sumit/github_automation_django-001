1492f842a99cea3f9f02e14854722dcc0eb10ddc
Sumit Sontakke <smsontakke@ymail.com>
2022-05-22 11:27:57 +0100

    updated privs

--- data.tf
+++ data.tf
@@ -11,7 +11,10 @@
 
 data "aws_iam_policy_document" "deployment-service-policy" {
   statement {
-    actions = ["sts:AssumeRole"]
-    service = "codedeploy.amazonaws.com"
+    actions = ["sts:AssumeRole", "autoscaling:*", "codedeploy:*", "ec2:*", "lambda:*", "ecs:*", "elasticloadbalancing:*", "iam:AddRoleToInstanceProfile", "iam:AttachRolePolicy", "iam:CreateInstanceProfile", "iam:CreateRole", "iam:DeleteInstanceProfile", "iam:DeleteRole", "iam:DeleteRolePolicy", "iam:GetInstanceProfile", "iam:GetRole", "iam:GetRolePolicy", "iam:ListInstanceProfilesForRole", "iam:ListRolePolicies", "iam:ListRoles", "iam:PutRolePolicy", "iam:RemoveRoleFromInstanceProfile", "s3:*", "ssm:*"]
+    principals {
+      type        = "Resource"
+      identifiers = ["*"]
+    }
   }
 }
