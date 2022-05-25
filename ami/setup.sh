661dd8944ac10c2b87f6c59aa5589869a64f8b6c
Sumit Sontakke <smsontakke@ymail.com>
2022-05-25 18:35:32 +0100

    install commands updated with yum for amazon linux  machine

--- ami/setup.sh
+++ ami/setup.sh
@@ -1,13 +1,13 @@
-apt-get update  
+yum update  
 echo "Install git"
-apt-get install -y git
+yum install -y git
 echo "Install Python3"
-apt-get install -y python3
+yum install -y python3
 echo "Install pip3"
-apt-get install -y python3-pip
+yum install -y python3-pip
 
 echo "Install postgresql-client"
-apt-get install -y postgresql-client
+yum install -y postgresql-client
 
 
 git clone --branch feature/cicd https://github.com/Er-sumit/github_automation_django-001.git /feature_src
