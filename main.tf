6c465d6cac4e80cb9c5a06122a412d9bba9f7314
Sumit Sontakke <smsontakke@ymail.com>
2022-05-28 09:34:53 +0100

    terraform pick ami and build isntance

--- main.tf
+++ main.tf
@@ -31,3 +31,59 @@ module "network" {
   }
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "162.198.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "162.198.0.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/django-app--*","django-app-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["245411993826"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  network_interface {
+    network_interface_id = module.network.public_subnets[0].id
+    device_index         = 0
+  }
+  tags = {
+    Name = "Django App"
+  }
+    
+
+}
