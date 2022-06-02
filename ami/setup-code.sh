c7c21d58ce78da95f13de5a7362eca6e0a01e977
Sumit Sontakke <smsontakke@ymail.com>
2022-06-02 09:03:42 +0100

    ami ubuntu image and django code

---
+++ ami/setup-code.sh
@@ -0,0 +1,14 @@
+sudo apt-get update -y
+sudo git clone --branch feature/cicd https://github.com/Er-sumit/github_automation_django-001.git /src
+me=`whoami`
+sudo chown -Rf $me /src
+sudo apt-get install python3-pip -y
+#pip3 install --upgrade pip
+pip3 install -r /tmp/requirements.txt
+cd /src
+django-admin startproject newproject
+cd /src/newproject
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo $APP_SECRET >> /src/newproject/newproject/.env
