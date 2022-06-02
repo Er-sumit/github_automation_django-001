412a0b762c2b2e69d2de3c918c3c7ac604228de1
Sumit Sontakke <smsontakke@ymail.com>
2022-06-02 09:39:44 +0100

    updating PATH

--- ami/setup-code.sh
+++ ami/setup-code.sh
@@ -1,8 +1,13 @@
+sleep 30
 sudo yum update -y
 git clone --branch feature/cicd https://github.com/Er-sumit/github_automation_django-001.git /src
 me=`whoami`
 sudo chown -Rf $me /src
 sudo yum install python3-pip -y
+export PATH=$PATH:/usr/local/bin
+echo "export PATH=$PATH:/usr/local/bin" >> ~/.bash_profile
+sleep 1
+source ~/.bash_profile
 #pip3 install --upgrade pip
 pip3 install -r /tmp/requirements.txt
 mkdir -p /src
